20ef42e4b17bf34d1f9b1fcf63a8b2578e422f60
esouza-dev <esouza.dev@outlook.com>
2023-01-04 00:24:17 -0300

    add table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  template: `<div>[{{message}}]</div>`,
+  template: `<div>[{{message}}]</div><br /><app-table></app-table>`,
 })
 export class AppComponent {
   message:string
-  constructor(private http: HttpClient) {
-    this.http.get('/api/message')
-      .subscribe((resp: any) => this.message = resp.text);
-  }
+  // constructor(private http: HttpClient) {
+  //   this.http.get('/api/message')
+  //     .subscribe((resp: any) => this.message = resp.text);
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from '@angular/common/http';
-
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from "./app.component";
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule,
-    HttpClientModule],
+  declarations: [AppComponent, TableComponent],
+  imports: [BrowserModule,BrowserAnimationsModule,
+    HttpClientModule,MatTableModule,MatIconModule,MatButtonModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
