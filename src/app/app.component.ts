33c50c0fb9ef1128ca9eb5851cfafd0758ad61ba
esouza-dev <esouza.dev@outlook.com>
2023-01-04 01:55:01 -0300

    add environment api location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
-  template: `<div>[{{message}}]</div><br /><app-table></app-table>`,
+  template: `<div>[{{message}}] </div><br /><app-table></app-table>`,
 })
 export class AppComponent {
   message:string
   constructor(private http: HttpClient) {
-    this.http.get('/api/message')
+    this.http.get(`${environment.apiUrl}/api/message`)
       .subscribe((resp: any) => this.message = resp.text);
   }
 }
