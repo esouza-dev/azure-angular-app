50acec438926568e4906279ecc05056114e691a7
esouza-dev <esouza.dev@outlook.com>
2023-01-04 02:10:29 -0300

    e

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { environment } from './../environments/environment';
 export class AppComponent {
   message:string
   constructor(private http: HttpClient) {
-    this.http.get(`${environment.apiUrl??""}/api/message`)
+    this.http.get(`${environment.apiUrl}/api/message`)
       .subscribe((resp: any) => this.message = resp.text);
   }
 }
